ec4187b1b9f0c537156b711915905c812d10b55f
zumxrx <zumarramzi@gmail.com>
2025-01-31 10:41:24 +0530

    New Commit

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+        'product_title',
+        'price',
+        'quantity',
+        'image',
+        'Product_id',
+        'user_id',
+    ];
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+        'user_id',
+        'product_title',
+        'quantity',
+        'price',
+        'image',
+        'product_id',
+        'payment_status',
+        'delivery_status',
+    ];
 }
